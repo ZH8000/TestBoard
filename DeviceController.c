72ed6c8b81b0d736dac6d13eb38bc51ba65140c9
Brian Hsu <brianhsu.hsu@gmail.com>
2016-04-14 09:44:40 +0800

    Change $c$ command to 3 state: charge, discharge, all disabled.

--- DeviceController.c
+++ DeviceController.c
@@ -11,7 +11,7 @@ void sendUUID() {
 	readFlashToBuffer(uuidString, 50, currentFlashDataEndAddress);
 	sendUART("#f#");
 	sendUART(uuidString);
-	sendUART("#\r\n");
+	sendUART("#\n");
 }
 
 void enableLCChannel(char channelCode) {
@@ -26,22 +26,22 @@ void enableLCChannel(char channelCode) {
 	// Enable coressponding LCR channel GPIO
 	switch(channelCode) {
 		case '0':
-			sendUART("#b#0#\r\n");
+			sendUART("#b#0#\n");
 			break;
 		case '1':
 			IO_vSetPin(LC_CHANNEL_1);
-			sendUART("#b#1#\r\n");
+			sendUART("#b#1#\n");
 			break;
 		case '2':
 			IO_vSetPin(LC_CHANNEL_2);
-			sendUART("#b#2#\r\n");		
+			sendUART("#b#2#\n");		
 			break;
 		case '3':
 			IO_vSetPin(LC_CHANNEL_3);
-			sendUART("#b#3#\r\n");
+			sendUART("#b#3#\n");
 			break;
 		default:
-			sendUART("Unknown LC Channel.\r\n");			
+			sendUART("#Unknown LC Channel.#\n");			
 			break;
 	}
 }
@@ -58,22 +58,22 @@ void enableLCRChannel(char channelCode) {
 	// Enable coressponding LCR channel GPIO
 	switch(channelCode) {
 		case '0':
-			sendUART("#a#0#\r\n");
+			sendUART("#a#0#\n");
 			break;
 		case '1':
 			IO_vSetPin(LCR_CHANNEL_1);
-			sendUART("#a#1#\r\n");
+			sendUART("#a#1#\n");
 			break;
 		case '2':
 			IO_vSetPin(LCR_CHANNEL_2);
-			sendUART("#a#2#\r\n");		
+			sendUART("#a#2#\n");		
 			break;
 		case '3':
 			IO_vSetPin(LCR_CHANNEL_3);
-			sendUART("#a#3#\r\n");
+			sendUART("#a#3#\n");
 			break;
 		default:
-			sendUART("Unknown LCR Channel.\r\n");			
+			sendUART("#Unknown LCR Channel.#\n");			
 			break;
 	}
 }
@@ -93,45 +93,87 @@ bool isAllHVRelayOK() {
 }
 
 void sendHVRelayStatus() {
-	if (isAllHVRelayOK()) {
-		sendUART("HV_OK\r\n");
+	if (IO_ubReadPin(HV_READY_1)) {
+		sendUART("HV_READY_1: OK\n");
 	} else {
-		sendUART("HV_NG\r\n");		
+		sendUART("HV_READY_1: NG\n");
+	}
+	
+	if (IO_ubReadPin(HV_READY_2)) {
+		sendUART("HV_READY_2: OK\n");
+	} else {
+		sendUART("HV_READY_2: NG\n");
+	}
+
+	if (IO_ubReadPin(HV_READY_3)) {
+		sendUART("HV_READY_3: OK\n");
+	} else {
+		sendUART("HV_READY_3: NG\n");
+	}
+	
+	if (IO_ubReadPin(HV_FAULT_1)) {
+		sendUART("HV_FAULT_1: OK\n");
+	} else {
+		sendUART("HV_FAULT_1: NG\n");
+	}
+
+	if (IO_ubReadPin(HV_FAULT_2)) {
+		sendUART("HV_FAULT_2: OK\n");
+	} else {
+		sendUART("HV_FAULT_2: NG\n");
+	}
+	
+	if (IO_ubReadPin(HV_FAULT_3)) {
+		sendUART("HV_FAULT_3: OK\n");
+	} else {
+		sendUART("HV_FAULT_3: NG\n");
+	}
+	
+	
+	if (isAllHVRelayOK()) {
+		sendUART("#d#1#\n");
+	} else {
+		sendUART("#d#0#\n");		
 	}
 }
 
 void setChargeMode(char modeCode) {
-	if (modeCode == '1') {
+	if (modeCode == '0') {
+		IO_vResetPin(HV_CHANNEL);		
+		IO_vSetPin(DISCHARGE_CHANNEL);
+		IO_vSetPin(CHARGE_CHANNEL);
+		sendUART("#c#0#\n");		
+	} else if (modeCode == '1') {
 		if (!isAllHVRelayOK()) {
 			IO_vResetPin(HV_CHANNEL);			
-			IO_vResetPin(CHARGE_CHANNEL);
-			IO_vResetPin(DISCHARGE_CHANNEL);
-			sendUART("HV_NG\r\n");
+			IO_vSetPin(CHARGE_CHANNEL);
+			IO_vSetPin(DISCHARGE_CHANNEL);
+			sendUART("#d#0#\n");
 			return;
 		}
-		IO_vResetPin(DISCHARGE_CHANNEL);
-		IO_vSetPin(HV_CHANNEL);
-		IO_vSetPin(CHARGE_CHANNEL);
-		sendUART("#c#1#\r\n");
-	} else if (modeCode == '0') {
-		IO_vResetPin(CHARGE_CHANNEL);
-		IO_vResetPin(HV_CHANNEL);
 		IO_vSetPin(DISCHARGE_CHANNEL);
-		sendUART("#c#0#\r\n");
+		IO_vSetPin(HV_CHANNEL);
+		IO_vResetPin(CHARGE_CHANNEL);
+		sendUART("#c#1#\n");
+	} else if (modeCode == '2') {
+		IO_vSetPin(CHARGE_CHANNEL);
+		IO_vResetPin(HV_CHANNEL);
+		IO_vResetPin(DISCHARGE_CHANNEL);
+		sendUART("#c#2#\n");
 	} else {
-		sendUART("Unknown Charge Mode\r\n");		
+		sendUART("#Unknown Charge Mode#\n");		
 	}
 }
 
 void setHVMonitorMode(char modeCode) {
 	if (modeCode == '1') {
 		shouldMonitorHVStatus = 1;
-		sendUART("#e#1#\r\n");
+		sendUART("#e#1#\n");
 	} else if (modeCode == '0') {
 		shouldMonitorHVStatus = 0;
-		sendUART("#e#0#\r\n");
+		sendUART("#e#0#\n");
 	} else {
-		sendUART("Unknown HV Monitor Code\r\n");
+		sendUART("#Unknown HV Monitor Code#\n");
 	}
 	
 }
@@ -142,8 +184,8 @@ void setUUID(char * command) {
 	strncpy(uuidString, command+3, 36);
 	sendUART("Setting UUID to ");
 	sendUART(uuidString);
-	sendUART("....\r\n");
+	sendUART("....\n");
 	sendUART("Please wait...");
 	writeBufferToFlash(uuidString, &flashEndAddress);
-	sendUART("DONE\r\n");
+	sendUART("DONE\n");
 }
