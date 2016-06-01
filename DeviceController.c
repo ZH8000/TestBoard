1e734f3ad0ed0d133a82c0e378ce201c34c5414c
Brian Hsu <brianhsu.hsu@gmail.com>
2016-06-01 10:25:41 +0800

    A clean version without UUID flash function.

---
+++ DeviceController.c
@@ -0,0 +1,178 @@
+#include "DeviceController.h"
+#include "PinDefine.h"
+#include "UART.h"
+#include <string.h>
+
+#define PREDEFINED_UUID		"bfcf9428-eef6-4261-ae2e-e9d66c7e3d31"
+extern bool shouldMonitorHVStatus;
+
+
+void sendUUID() {
+    sendUART("#f#");
+    sendUART(PREDEFINED_UUID);
+    sendUART("#\n");
+}
+
+void enableLCChannel(char channelCode) {
+    // Switch off all LC / LCR channel first.
+    IO_vResetPin(LCR_CHANNEL_1);
+    IO_vResetPin(LCR_CHANNEL_2);
+    IO_vResetPin(LCR_CHANNEL_3);    
+    IO_vResetPin(LC_CHANNEL_1);
+    IO_vResetPin(LC_CHANNEL_2);
+    IO_vResetPin(LC_CHANNEL_3);
+
+    // Enable coressponding LCR channel GPIO
+    switch(channelCode) {
+        case '0':
+            sendUART("#b#0#\n");
+            break;
+        case '1':
+            IO_vSetPin(LC_CHANNEL_1);
+            sendUART("#b#1#\n");
+            break;
+        case '2':
+            IO_vSetPin(LC_CHANNEL_2);
+            sendUART("#b#2#\n");            
+            break;
+        case '3':
+            IO_vSetPin(LC_CHANNEL_3);
+            sendUART("#b#3#\n");
+            break;
+        default:
+            sendUART("#Unknown LC Channel.#\n");                    
+            break;
+    }
+}
+
+void enableLCRChannel(char channelCode) {
+    // Switch off all LC / LCR channel first.
+    IO_vResetPin(LCR_CHANNEL_1);
+    IO_vResetPin(LCR_CHANNEL_2);
+    IO_vResetPin(LCR_CHANNEL_3);
+    IO_vResetPin(LC_CHANNEL_1);
+    IO_vResetPin(LC_CHANNEL_2);
+    IO_vResetPin(LC_CHANNEL_3);
+
+    // Enable coressponding LCR channel GPIO
+    switch(channelCode) {
+        case '0':
+            sendUART("#a#0#\n");
+            break;
+        case '1':
+            IO_vSetPin(LCR_CHANNEL_1);
+            sendUART("#a#1#\n");
+            break;
+        case '2':
+            IO_vSetPin(LCR_CHANNEL_2);
+            sendUART("#a#2#\n");            
+            break;
+        case '3':
+            IO_vSetPin(LCR_CHANNEL_3);
+            sendUART("#a#3#\n");
+            break;
+        default:
+            sendUART("#Unknown LCR Channel.#\n");                   
+            break;
+    }
+}
+
+bool isAllHVRelayOK() {
+#ifdef STARTER_KIT_EMU
+    return IO_ubReadPin(HV_READY_1);
+#else
+    return 
+        IO_ubReadPin(HV_READY_1) &&
+        IO_ubReadPin(HV_READY_2) &&
+        IO_ubReadPin(HV_READY_3) &&
+        IO_ubReadPin(HV_FAULT_1) &&
+        IO_ubReadPin(HV_FAULT_2) &&
+        IO_ubReadPin(HV_FAULT_3);
+#endif
+}
+
+void sendHVRelayStatus() {
+    if (IO_ubReadPin(HV_READY_1)) {
+        sendUART("HV_READY_1: OK\n");
+    } else {
+        sendUART("HV_READY_1: NG\n");
+    }
+
+    if (IO_ubReadPin(HV_READY_2)) {
+        sendUART("HV_READY_2: OK\n");
+    } else {
+        sendUART("HV_READY_2: NG\n");
+    }
+
+    if (IO_ubReadPin(HV_READY_3)) {
+        sendUART("HV_READY_3: OK\n");
+    } else {
+        sendUART("HV_READY_3: NG\n");
+    }
+
+    if (IO_ubReadPin(HV_FAULT_1)) {
+        sendUART("HV_FAULT_1: OK\n");
+    } else {
+        sendUART("HV_FAULT_1: NG\n");
+    }
+
+    if (IO_ubReadPin(HV_FAULT_2)) {
+        sendUART("HV_FAULT_2: OK\n");
+    } else {
+        sendUART("HV_FAULT_2: NG\n");
+    }
+
+    if (IO_ubReadPin(HV_FAULT_3)) {
+        sendUART("HV_FAULT_3: OK\n");
+    } else {
+        sendUART("HV_FAULT_3: NG\n");
+    }
+
+
+    if (isAllHVRelayOK()) {
+        sendUART("#d#1#\n");
+    } else {
+        sendUART("#d#0#\n");            
+    }
+}
+
+void setChargeMode(char modeCode) {
+    if (modeCode == '0') {
+        IO_vResetPin(HV_CHANNEL);               
+        IO_vSetPin(DISCHARGE_CHANNEL);
+        IO_vSetPin(CHARGE_CHANNEL);
+        sendUART("#c#0#\n");            
+    } else if (modeCode == '1') {
+        if (!isAllHVRelayOK()) {
+            IO_vResetPin(HV_CHANNEL);                       
+            IO_vSetPin(CHARGE_CHANNEL);
+            IO_vSetPin(DISCHARGE_CHANNEL);
+            sendUART("#d#0#\n");
+            return;
+        }
+        IO_vSetPin(DISCHARGE_CHANNEL);
+        IO_vSetPin(HV_CHANNEL);
+        IO_vResetPin(CHARGE_CHANNEL);
+        sendUART("#c#1#\n");
+    } else if (modeCode == '2') {
+        IO_vSetPin(CHARGE_CHANNEL);
+        IO_vResetPin(HV_CHANNEL);
+        IO_vResetPin(DISCHARGE_CHANNEL);
+        sendUART("#c#2#\n");
+    } else {
+        sendUART("#Unknown Charge Mode#\n");            
+    }
+}
+
+void setHVMonitorMode(char modeCode) {
+    if (modeCode == '1') {
+        shouldMonitorHVStatus = 1;
+        sendUART("#e#1#\n");
+    } else if (modeCode == '0') {
+        shouldMonitorHVStatus = 0;
+        sendUART("#e#0#\n");
+    } else {
+        sendUART("#Unknown HV Monitor Code#\n");
+    }
+
+}
---
+++ PinDefine.h
@@ -0,0 +1,75 @@
+#ifndef __PIN_DEFINE_H__
+#define __PIN_DEFINE_H__
+
+    //#define STARTER_KIT_EMU
+    #ifdef STARTER_KIT_EMU
+        #define LCR_CHANNEL_1                   P3_0
+        #define LCR_CHANNEL_2                   P3_2
+        #define LCR_CHANNEL_3                   P3_4
+    
+        #define LC_CHANNEL_1                    P3_1
+        #define LC_CHANNEL_2                    P3_3
+        #define LC_CHANNEL_3                    P3_5
+    
+        #define HV_READY_1                      P4_0
+        #define HV_READY_2                      P4_2
+        #define HV_READY_3                      P4_4
+    
+        #define HV_FAULT_1                      P4_1
+        #define HV_FAULT_2                      P4_3
+        #define HV_FAULT_3                      P4_5
+    
+        #define HV_CHANNEL                      P3_6
+        #define CHARGE_CHANNEL                  P3_7
+        #define DISCHARGE_CHANNEL               P1_2
+    #else
+        #define LCR_CHANNEL_1                   P1_0
+        #define LCR_CHANNEL_2                   P1_3
+        #define LCR_CHANNEL_3                   P1_4
+    
+        #define LC_CHANNEL_1                    P1_1
+        #define LC_CHANNEL_2                    P1_2
+        #define LC_CHANNEL_3                    P1_5
+    
+        #define HV_READY_1                      P4_0
+        #define HV_READY_2                      P4_2
+        #define HV_READY_3                      P4_4
+    
+        #define HV_FAULT_1                      P4_1
+        #define HV_FAULT_2                      P4_3
+        #define HV_FAULT_3                      P4_5
+    
+        #define HV_CHANNEL                      P3_7
+        #define CHARGE_CHANNEL                  P3_4
+        #define DISCHARGE_CHANNEL               P3_6
+    #endif
+
+#define SET_LCR_CHANNEL_1                       LCR_CHANNEL_1 = 1
+#define SET_LCR_CHANNEL_2                       LCR_CHANNEL_2 = 1
+#define SET_LCR_CHANNEL_3                       LCR_CHANNEL_3 = 1
+#define SET_LC_CHANNEL_1                        LC_CHANNEL_1 = 1
+#define SET_LC_CHANNEL_2                        LC_CHANNEL_2 = 1
+#define SET_LC_CHANNEL_3                        LC_CHANNEL_3 = 1
+#define SET_HV_CHANNEL                          HV_CHANNEL = 1
+#define SET_CHARGE_CHANNEL                      CHARGE_CHANNEL = 1
+#define SET_DISCHARGE_CHANNEL                   DISCHARGE_CHANNEL = 1
+
+#define RESET_LCR_CHANNEL_1                     LCR_CHANNEL_1 = 0
+#define RESET_LCR_CHANNEL_2                     LCR_CHANNEL_2 = 0
+#define RESET_LCR_CHANNEL_3                     LCR_CHANNEL_3 = 0
+#define RESET_LC_CHANNEL_1                      LC_CHANNEL_1 = 0
+#define RESET_LC_CHANNEL_2                      LC_CHANNEL_2 = 0
+#define RESET_LC_CHANNEL_3                      LC_CHANNEL_3 = 0
+#define RESET_HV_CHANNEL                        HV_CHANNEL = 0
+#define RESET_CHARGE_CHANNEL                    CHARGE_CHANNEL = 0
+#define RESET_DISCHARGE_CHANNEL                 DISCHARGE_CHANNEL = 0
+
+#define READ_HV_READY_1                         HV_READY_1
+#define READ_HV_READY_2                         HV_READY_2
+#define READ_HV_READY_3                         HV_READY_3
+
+#define READ_HV_FAULT_1                         HV_FAULT_1
+#define READ_HV_FAULT_2                         HV_FAULT_2
+#define READ_HV_FAULT_3                         HV_FAULT_3
+
+#endif
