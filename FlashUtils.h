fd02a79017dc0712910e97c99f886e996a61681b
Brian Hsu <brianhsu.hsu@gmail.com>
2016-06-01 10:25:20 +0800

    Cleanup all files.

--- FlashUtils.h
+++
@@ -1,9 +0,0 @@
-#ifndef __FLASH_UTILS_H__
-#define __FLASH_UTILS_H__
-
-unsigned char code * getFlashDataEndAddress();
-void writeToFlashPage(char xdata * buffer, unsigned char code ** EECurrentAddr);
-void readFlashToBuffer(char xdata * buffer, int n, unsigned char code * EECurrentAddr);
-void writeBufferToFlash(char * buffer, unsigned char code ** EECurrentAddr);
-
-#endif
